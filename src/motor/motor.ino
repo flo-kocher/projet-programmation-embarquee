ba0fe0405219dd069a54826875904c12a302d5dc
Thibault BRARD <brard.thibault0@gmail.com>
2022-12-28 21:16:53 +0100

    Le moteur fonctionne avec le potentiomètre

--- src/motor/motor.ino
+++ src/motor/motor.ino
@@ -1,36 +1,54 @@
+#define PERIODE_AFFICHAGE       1000000  
+
 const int controlPin1 = 2;                // connected to pin 7 on the H-bridge
 const int controlPin2 = 3;                // connected to pin 2 on the H-bridge
 const int enablePin = 9;                  // connected to pin 1 on the H-bridge
 const int potPin = A0;                    // connected to the potentiometer's output
 
-int motorSpeed = 0;      // speed of the motor
+static unsigned long ulmicroseconds = 0;
+static unsigned long ulPrecMicroseconds = 0;
 
+int motorSpeed = 0;      // speed of the motor
 void setup() {
   // initialize the inputs and outputs
   pinMode(controlPin1, OUTPUT);
   pinMode(controlPin2, OUTPUT);
   pinMode(enablePin, OUTPUT);
+  pinMode(A0, INPUT);
 
   // pull the enable pin LOW to start
   digitalWrite(enablePin, LOW);
+  Serial.begin(115200); 
 }
 
 void loop() {
 
   // read the value of the pot and divide by 4 to get a value that can be
   // used for PWM
-  motorSpeed = analogRead(potPin) / 4;
-
+  int potValue = analogRead(potPin);
   // change the direction the motor spins by talking to the control pins
   // on the H-Bridge
-  if (motorDirection == 1) {
+  //en avant
+  if (potValue <= 511) {
+    motorSpeed = (abs(potValue-511))/2;
     digitalWrite(controlPin1, HIGH);
     digitalWrite(controlPin2, LOW);
-  } else {
+  } else { //en arrière
+    motorSpeed = (potValue - 511)/2;
     digitalWrite(controlPin1, LOW);
     digitalWrite(controlPin2, HIGH);
   }
 
+  ulmicroseconds = micros();
+  if(ulmicroseconds-ulPrecMicroseconds >= PERIODE_AFFICHAGE)
+  {
+    Serial.print("Valeur du potentiomètre : ");
+    Serial.println(potValue);
+    Serial.print("speed : ");
+    Serial.println(motorSpeed);
+
+    ulPrecMicroseconds = ulmicroseconds;
+  }
   analogWrite(enablePin, motorSpeed);
 
 }
